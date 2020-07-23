c4e586280cfa18919b163110a0bd77e7eb50a60a
krishna <krishnachittodia.10@gmail.com>
2020-07-24 04:08:54 +0530

    added profile and editor role

--- server/server.js
+++ server/server.js
@@ -85,3 +85,15 @@ app.models.Role.find({where: {name: 'admin'}}, (err, role) => {
     }
   }
 });
+
+app.models.Role.find({where: {name: 'editor'}}, (err, roles) => {
+  if (!err && roles) {
+    if (roles.length === 0) {
+      app.models.Role.create({
+        name: 'editor',
+      }, (err2, result) => {
+        console.log(err2, result);
+      });
+    }
+  }
+});
